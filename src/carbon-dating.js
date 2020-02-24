b96d4490c232a894fcd3edb94ced2d6d2bc02141
vpikta94 <vpikta94@gmail.com>
2020-02-24 18:31:16 +0300

    feat: completed cat and carbon tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,15 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+
+  let parsedVal;
+  if (typeof sampleActivity === 'string') {
+    parsedVal = parseFloat(sampleActivity);
+  } else return false;
+
+  if (parsedVal > 0 && parsedVal < MODERN_ACTIVITY) {
+    return Math.ceil(Math.log(MODERN_ACTIVITY / parsedVal) * HALF_LIFE_PERIOD / 0.693);
+  }
+  return false;
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,7 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let count = 0;
+  matrix.flat().forEach(element => {
+    if (element == '^^') count++
+  });
+  return count;
 };
